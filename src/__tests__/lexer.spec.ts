08d4777ac079540d197efc0f299b3076e081d388
Robert Grindeland <rgrindeland@gmail.com>
2017-09-20 10:33:37 -0500

    Set up test scripts

--- src/__tests__/lexer.spec.ts
+++ src/__tests__/lexer.spec.ts
@@ -1,44 +1,13 @@
-import Compiler from '../Compiler';
+import { TOKEN_TYPE } from '../constants';
 import Lexer from '../Lexer';
-import Parser from '../Parser';
 
-import { compileJS, minify } from '../';
+it('Ignores whitespace characters', () => {
+    const tab = String.fromCharCode(9);
+    const space = ' ';
+    const newline = '\n';
 
-import * as fs from 'fs';
+    const lexer = new Lexer(tab + space + newline);
+    const result = lexer.analyze();
 
-const inputFile = fs.readFileSync(__dirname + '/../../examples/rare_world_drops.loot', 'utf8');
-
-// fs.writeFileSync(__dirname + '/compiled.js', compileJS(inputFile));
-// console.log(minify(inputFile).length);
-// console.log(compileJS(inputFile).length);
-// console.log(compileJS(minify(inputFile)).length);
-
-// console.log(minify(inputFile));
-
-const lexer = new Lexer(inputFile);
-const parser = new Parser(lexer.analyze());
-const compiler = new Compiler(parser.parse());
-
-const compiled = compiler.compile();
-fs.writeFileSync(__dirname + '/compiled.js', compiled);
-
-const result = require(__dirname + '/compiled.js');
-
-console.log(result.test());
-
-// const fn = eval(compiled.test);
-// const res = {
-//     MINE: 0,
-//     YOURS: 0,
-//     item2: 0
-// };
-
-// for (let i = 0; i < 1e6; i++) {
-//     const type = fn().type;
-//     res[type]++;
-//     // console.log(type);
-// }
-
-// console.log(res);
-
-// console.log(minify(inputFile));
+    expect(result.length).toEqual(0);
+});
